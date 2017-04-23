14a3abe7ce7e9fabd53fa45568b39d833d52a989
Henry Chu <henrytech@outlook.com>
2017-04-23 09:29:03 +0800

    added admin and passenger controllers

---
+++ src/main/java/com/sustech/flightbooking/controllers/AdminController.java
@@ -0,0 +1,11 @@
+package com.sustech.flightbooking.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Henry on 4/23/2017.
+ */
+
+@Controller
+public class AdminController {
+}
---
+++ src/main/java/com/sustech/flightbooking/controllers/PassengerController.java
@@ -0,0 +1,12 @@
+package com.sustech.flightbooking.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Henry on 4/23/2017.
+ */
+
+@Controller
+public class PassengerController {
+    
+}
