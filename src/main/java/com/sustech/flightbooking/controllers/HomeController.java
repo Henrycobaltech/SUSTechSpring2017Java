ff9d476f109aa0b56034f38f618caa1758117330
Henry Chu <henrytech@outlook.com>
2017-04-24 00:21:53 +0800

    added login action

--- src/main/java/com/sustech/flightbooking/controllers/HomeController.java
+++ src/main/java/com/sustech/flightbooking/controllers/HomeController.java
@@ -1,7 +1,10 @@
 package com.sustech.flightbooking.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
 
 /**
  * Created by Henry on 4/16/2017.
@@ -11,7 +14,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     @RequestMapping("/")
-    public String index() {
+    public String index(ModelMap modelMap) {
+        modelMap.put("now", new Date());
         return "index";
     }
 }
--- src/main/java/com/sustech/flightbooking/controllers/PassengerController.java
+++ src/main/java/com/sustech/flightbooking/controllers/PassengerController.java
@@ -1,12 +1,19 @@
 package com.sustech.flightbooking.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by Henry on 4/23/2017.
  */
 
 @Controller
+@RequestMapping("/passengers")
 public class PassengerController {
-    
+
+    @GetMapping("/login")
+    public String login() {
+        return "passengers/login";
+    }
 }
